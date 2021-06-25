375d8c1a0cc1f4ee03f23c83956f544a27207ef8
Daniel Yapura <danielyapura@gmail.com>
2021-06-25 10:41:55 -0300

    Fix importación

---
+++ src/main/java/com/jdyapura/api/disney/controllers/MovieController.java
@@ -0,0 +1,28 @@
+package com.jdyapura.api.disney.controllers;
+
+import com.jdyapura.api.disney.models.Movie;
+import com.jdyapura.api.disney.services.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieController {
+
+
+    @Autowired
+    private MovieService service;
+
+    @GetMapping
+    public ResponseEntity<Iterable<Movie>> getAllMovies() {
+
+        return new ResponseEntity(service.findAllMovies(), HttpStatus.OK);
+    }
+
+}
