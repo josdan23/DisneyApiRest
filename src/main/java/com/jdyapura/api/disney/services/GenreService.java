3a634efbf62d886e5fcf0cab867c722df197157d
Daniel Yapura <danielyapura@gmail.com>
2021-06-26 01:05:19 -0300

    Se implemente el genreServices con metodos básicos

---
+++ src/main/java/com/jdyapura/api/disney/services/GenreService.java
@@ -0,0 +1,49 @@
+package com.jdyapura.api.disney.services;
+
+import com.jdyapura.api.disney.models.Genre;
+import com.jdyapura.api.disney.repositories.GenreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class GenreService {
+
+    @Autowired
+    private GenreRepository repository;
+
+    public List<Genre> findAllGenres() {
+        return repository.findAll();
+    }
+
+    public Genre findGenreById(int idGenre) {
+        Optional<Genre> result = repository.findById(idGenre);
+        if (!result.isPresent())
+            return null;
+        return result.get();
+    }
+
+    public Genre saveGenre(Genre newGenre) {
+        return repository.save(newGenre);
+    }
+
+    public Genre updateGenre(int idGenre, Genre updatedGenre) {
+        Genre savedGenre = findGenreById(idGenre);
+        if(savedGenre == null)
+            return null;
+
+        savedGenre.setName(updatedGenre.getName());
+        savedGenre.setImage(updatedGenre.getImage());
+        return repository.save(savedGenre);
+    }
+
+    public void deleteGenre(int idGenre) {
+        if (!repository.existsById(idGenre))
+            return;
+        
+        repository.deleteById(idGenre);
+    }
+
+}
