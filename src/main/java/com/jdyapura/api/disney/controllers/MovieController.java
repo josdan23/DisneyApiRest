7acbe851ec675be793833419cd9f451feac57210
Daniel Yapura <danielyapura@gmail.com>
2021-06-27 12:33:26 -0300

    Agregado endopoint para recuperar un movie

--- src/main/java/com/jdyapura/api/disney/controllers/MovieController.java
+++ src/main/java/com/jdyapura/api/disney/controllers/MovieController.java
@@ -2,10 +2,12 @@ package com.jdyapura.api.disney.controllers;
 
 import com.jdyapura.api.disney.models.Movie;
 import com.jdyapura.api.disney.services.MovieService;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,4 +27,8 @@ public class MovieController {
         return new ResponseEntity(service.findAllMovies(), HttpStatus.OK);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Movie> getMovieById(@PathVariable("id") int idMovie) {
+        return new ResponseEntity<>(service.findMovieById(idMovie), HttpStatus.OK);
+    }
 }
