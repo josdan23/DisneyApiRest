be748d3e812022f162caa7cee3a1d1f074264c42
Daniel Yapura <danielyapura@gmail.com>
2021-07-08 18:05:16 -0300

    Se crean datos falsos de genero para llenar la tabla

--- src/main/java/com/jdyapura/api/disney/DisneyApplication.java
+++ src/main/java/com/jdyapura/api/disney/DisneyApplication.java
@@ -1,9 +1,15 @@
 package com.jdyapura.api.disney;
 
+import com.github.javafaker.Faker;
+import com.jdyapura.api.disney.entities.Genre;
+import com.jdyapura.api.disney.entities.Movie;
 import com.jdyapura.api.disney.services.GenreService;
+import com.jdyapura.api.disney.services.MovieService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class DisneyApplication {
@@ -12,17 +18,20 @@ public class DisneyApplication {
 		ConfigurableApplicationContext context = SpringApplication.run(DisneyApplication.class, args);
 
 		GenreService genreService = context.getBean(GenreService.class);
+		MovieService movieService = context.getBean(MovieService.class);
 
-		try {
-			System.out.println( genreService.findAllGenres());
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
+		Faker faker = new Faker();
+		for (int i = 0 ; i < 10 ; i++ ) {
+			try {
+				genreService.saveGenre(new Genre(faker.book().genre(), "no image"));
+
+			} catch (RuntimeException e) {
+
+			}
+			finally {
+				continue;
+			}
 		}
-
-/*
-		FakerService fakerService = context.getBean(FakerService.class);
-		fakerService.inserFakeDataDb();*/
-
 	}
 
 }
