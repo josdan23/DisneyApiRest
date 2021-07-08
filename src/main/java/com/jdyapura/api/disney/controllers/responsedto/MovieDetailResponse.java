8a8d9c37a093c539c8eff2af2ab2051187761c51
Daniel Yapura <danielyapura@gmail.com>
2021-07-08 15:40:38 -0300

    Se modificaron las clases response para implementar la inicializacion de los mismos

--- src/main/java/com/jdyapura/api/disney/controllers/responsedto/MovieDetailResponse.java
+++ src/main/java/com/jdyapura/api/disney/controllers/responsedto/MovieDetailResponse.java
@@ -1,14 +1,81 @@
 package com.jdyapura.api.disney.controllers.responsedto;
 
+import com.jdyapura.api.disney.entities.Movie;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class MovieDetailResponse {
-    public Integer id;
-    public String title;
-    public String creationDate;
-    public int qualifiers;
-    public String image;
-    public String genre;
-    public List<String> characters = new ArrayList<>();
+    private Integer id;
+    private String title;
+    private String creationDate;
+    private int calification;
+    private String image;
+    private String genre;
+    private List<String> characters = new ArrayList<>();
+
+    public MovieDetailResponse(Movie movie) {
+        this.id = movie.getIdMovie();
+        this.title = movie.getTitle();
+        this.creationDate = movie.getCreationDate().toString();
+        this.calification = movie.getCalification();
+        this.image = movie.getImage();
+        this.genre = movie.getGenre().getName();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(String creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public int getCalification() {
+        return calification;
+    }
+
+    public void setCalification(int calification) {
+        this.calification = calification;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public List<String> getCharacters() {
+        return characters;
+    }
+
+    public void setCharacters(List<String> characters) {
+        this.characters = characters;
+    }
 }
--- src/main/java/com/jdyapura/api/disney/controllers/responsedto/MovieResponse.java
+++ src/main/java/com/jdyapura/api/disney/controllers/responsedto/MovieResponse.java
@@ -1,7 +1,39 @@
 package com.jdyapura.api.disney.controllers.responsedto;
 
+import com.jdyapura.api.disney.entities.Movie;
+
 public class MovieResponse {
-    public String title;
-    public String image;
-    public String creationDate;
+    private String title;
+    private String image;
+    private String creationDate;
+
+    public MovieResponse(Movie movie) {
+        this.title = movie.getTitle();
+        this.image = movie.getImage();
+        this.creationDate = movie.getCreationDate().toString();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(String creationDate) {
+        this.creationDate = creationDate;
+    }
 }
