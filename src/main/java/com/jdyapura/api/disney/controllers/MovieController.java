d3d4495b2ce2debdbac510ad767d9f7e7e0e78fc
Daniel Yapura <danielyapura@gmail.com>
2021-07-12 16:36:05 -0300

    Acutaliza el metodo post para crear movies

--- src/main/java/com/jdyapura/api/disney/controllers/MovieController.java
+++ src/main/java/com/jdyapura/api/disney/controllers/MovieController.java
@@ -1,27 +1,42 @@
 package com.jdyapura.api.disney.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jdyapura.api.disney.controllers.responsedto.MovieDetailResponse;
 import com.jdyapura.api.disney.controllers.responsedto.MovieResponse;
 import com.jdyapura.api.disney.entities.Character;
+import com.jdyapura.api.disney.entities.Genre;
 import com.jdyapura.api.disney.entities.Movie;
+import com.jdyapura.api.disney.services.GenreService;
 import com.jdyapura.api.disney.services.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @RestController
-@RequestMapping("/movies")
+@RequestMapping
 public class MovieController {
 
+    private final String PATH_IMAGES = "./src/main/resources/static/";
 
     @Autowired
     private MovieService service;
 
-    @GetMapping
+    @Autowired
+    private GenreService genreService;
+
+    @GetMapping("/movies")
     public ResponseEntity<?> getAllMovies(
             @RequestParam(value = "name", required = false) String name,
             @RequestParam(value = "genre", required = false) Integer idGenre,
@@ -37,7 +52,7 @@ public class MovieController {
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/movies/{id}")
     public ResponseEntity<MovieDetailResponse> getMovieById(@PathVariable("id") int idMovie) {
 
         Movie savedMovie = service.findMovieById(idMovie);
@@ -53,12 +68,53 @@ public class MovieController {
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
-    @PostMapping
-    public ResponseEntity<Movie> postMovie(@RequestBody Movie movie) {
-        return new ResponseEntity<>(service.saveMovie(movie), HttpStatus.CREATED);
+    @PostMapping("/genres/{idGenre}/movies")
+    public ResponseEntity<?> postMovie(
+            @PathVariable("idGenre") Integer idGenre,
+            @RequestParam String data,
+            @RequestParam(required = false) MultipartFile imageFile) {
+
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+            Movie movie = mapper.readValue(data, Movie.class);
+
+            String dateFormated = LocalDate.parse(movie.getCreationDate()).toString();
+            movie.setCreationDate(dateFormated);
+
+            Genre genre = genreService.findGenreById(idGenre);
+            movie.setGenre( genre );
+
+
+            if (imageFile != null && !imageFile.isEmpty()) {
+
+                byte[] bytes = imageFile.getBytes();
+                Path path = Paths.get(PATH_IMAGES + imageFile.getOriginalFilename());
+                Files.write(path, bytes);
+                movie.setImage(path.toString());
+
+            } else {
+                movie.setImage("no image");
+            }
+
+            Movie savedMovie = service.saveMovie(movie);
+            System.out.println(movie);
+            System.out.println(savedMovie);
+
+            return new ResponseEntity<>(savedMovie, HttpStatus.CREATED);
+
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        } catch (RuntimeException e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        } catch (IOException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/movies/{id}")
     public ResponseEntity<Void> deleteMovie(@PathVariable("id") int idMovie) {
 
         service.deleteMovie(idMovie);
